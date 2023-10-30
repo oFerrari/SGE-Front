cd02067e8023dcd28005e135dbf6ee9a912c4488
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-10-30 19:55:30 -0300

    Criando tela de login

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
