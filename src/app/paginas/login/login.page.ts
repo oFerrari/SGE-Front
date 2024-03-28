957382eccded18626a20caa119e41d266317c46e
oFerrari <andrei.domingos@estudante.ifms.edu.br>
2024-03-27 22:42:08 -0400

    Logando server painel admin

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -19,13 +19,17 @@ export class LoginPage implements OnInit {
     this.menu.enable(true);
   }
 
+  logar() {
+    window.open('http://localhost:4200/', '_self');
+  }
+  
   /* logar() {
       this.nav.navigateForward('tabs/tab1');
   } */
 
-  logar() {
+  /* logar() {
       this.nav.navigateForward('painel-administrador');
-  }
+  } */
 
   registrar() {
     this.nav.navigateForward('add-edit-administrador');
