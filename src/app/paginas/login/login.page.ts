83ec621525d7258a0ebb81d2c146997488c0e6a6
andrei.domingos@estudante.ifms.edu.br <07180871192@ifms.edu.br>
2024-09-03 19:13:57 -0400

    att

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -10,7 +10,7 @@ import { MenuController, NavController } from '@ionic/angular';
 export class LoginPage implements OnInit {
   administradorForm: FormGroup;
 
-  private emailPredefinido: string = 'andrei.vinicius@estudante.ifms.edu.br ';
+  private emailPredefinido: string = 'andrei.vinicius@estudante.ifms.edu.br';
   private senhaPredefinida: string = '123456';
 
   constructor(public nav: NavController,
